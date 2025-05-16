662e84eea7f30250c8adcbb116159752461a2ca8
kmos <gpanice@redhat.com>
2025-05-16 18:58:13 +0200

    DBZ-8902 move in different packages

--- quarkus-debezium-parent/quarkus-debezium-engine-parent/runtime/src/main/java/io/quarkus/debezium/engine/capture/DebeziumCapturingHandler.java
+++ quarkus-debezium-parent/quarkus-debezium-engine-parent/runtime/src/main/java/io/quarkus/debezium/engine/capture/DebeziumCapturingHandler.java
@@ -4,14 +4,14 @@
  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package io.quarkus.debezium.engine;
+package io.quarkus.debezium.engine.capture;
+
+import java.util.function.Consumer;
 
 import org.apache.kafka.connect.source.SourceRecord;
 
 import io.debezium.engine.RecordChangeEvent;
 
-public interface CapturingInvoker {
-    void capture(RecordChangeEvent<SourceRecord> event);
+public interface DebeziumCapturingHandler extends Consumer<RecordChangeEvent<SourceRecord>> {
 
-    String getFullyQualifiedTableName();
 }
